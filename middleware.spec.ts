23fc65299a4040ea9b705a31e4dfd907e06c6440
Larissa Maza <larissa.maza@sumup.com>
2023-07-26 18:32:31 +0200

    wip

---
+++ middleware.spec.ts
@@ -0,0 +1,8 @@
+describe("middleware", () => {
+  describe("test", () => {
+    it("random assertion", async () => {
+      const url = "https://www.url.com/";
+      expect(url).toBe(url);
+    });
+  });
+});
--- middleware.ts
+++ middleware.ts
@@ -1,15 +1,16 @@
-import { NextRequest } from 'next/server';
-import { selectActiveExperiments } from '@ninetailed/experience.js';
-console.log(selectActiveExperiments)
+import { NextRequest } from "next/server";
+import { selectActiveExperiments } from "@ninetailed/experience.js";
+import { get } from "@vercel/edge-config";
+
+console.log(selectActiveExperiments);
 // comment import above to remove the issue
 // error Error [TypeError]: labels.entries is not a function or its return value is not iterable
 
 export async function middleware(req: NextRequest) {
+  console.log(get("test"));
   console.log(req.nextUrl);
 }
 
 export const config = {
-  matcher: [
-    '/((?!api|_next|favicon.ico|vercel.svg).*)'
-  ]
+  matcher: ["/((?!api|_next|favicon.ico|vercel.svg).*)"],
 };
